01c47c545eb8d4c8114da3da689b83735a6e5228
SuranartN <suranartnc@gmail.com>
2016-03-06 14:38:27 +0700

    implement scroll-behaviour

--- src/client.js
+++ src/client.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router, browserHistory } from 'react-router';
+import useScroll from 'scroll-behavior/lib/useStandardScroll';
+
+import { Router, browserHistory, useRouterHistory } from 'react-router';
 import routes from './routes';
 
-import { Provider } from 'react-redux';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+const createScrollHistory = useScroll(createBrowserHistory);
+const appHistory = useRouterHistory(createScrollHistory)();
 
+import { Provider } from 'react-redux';
 import createStore from './createStore';
 
 ReactDOM.render(
 	<Provider store={createStore(window.__INITIAL_STATE__)}>
-		<Router routes={routes} history={browserHistory} />
+		<Router routes={routes} history={appHistory} />
 	</Provider>
 	, document.getElementById('app'));
