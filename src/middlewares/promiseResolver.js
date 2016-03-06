bc6f31f39f0abafcedac5be820b26545448dfabb
SuranartN <suranartnc@gmail.com>
2016-03-06 14:24:37 +0700

    improve promise resolver middleware

--- src/middlewares/promiseResolver.js
+++ src/middlewares/promiseResolver.js
@@ -1,18 +1,26 @@
-export default store => next => action => {
-	
-	const { promise, ...rest } = action;
-
-	if (!promise) {
-		return next(action);
-	}
-
-	return promise.then(response => {
-    	if (response.status >= 400) {
-      		throw new Error("Bad response from server");
-      		return false;
-    	}
-    	return response.json();
-  	}).then(data => {
-    	return next({...rest, data});
-  	});
+export default function promiseMiddleware() {
+  return next => action => {
+    const { promise, type, ...rest } = action;
+   
+    if (!promise) return next(action);
+   
+    const SUCCESS = type;
+    const REQUEST = type + '_REQUEST';
+    const FAILURE = type + '_FAILURE';
+    next({ ...rest, type: REQUEST });
+    
+    return promise
+      .then(function(response) {
+        if (response.status >= 400) {
+          next({ ...rest, error: response.status, type: FAILURE });
+          console.log(response.status);
+          return false;
+        }
+        return response.json();
+      })
+      .then(data => {
+        next({ ...rest, data, type: SUCCESS });
+        return true;
+      });
+   };
 }
--- src/reducers/articleActiveReducer.js
+++ src/reducers/articleActiveReducer.js
@@ -1,7 +1,11 @@
-export default function(state = {}, action) {
+const initialState = {};
+
+export default function(state = initialState, action) {
 	switch(action.type) {
 		case 'GET_ARTICLE_BY_ID':
 			return action.data || state;
+		case 'GET_ARTICLE_LATEST':
+			return initialState;
 		default:
 			return state;
 	}
