0fc6086bb8cc91a381a5fb7f17c12d8db8f8b541
SuranartN <suranartnc@gmail.com>
2016-03-27 14:39:06 +0700

    make page transition supports for server rendering

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import Helmet from "react-helmet";
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-require('./global.css');
+import './global.css';
 import CSSModules from 'react-css-modules';
 import styles from './styles.css';
 
@@ -14,27 +13,6 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
-	}
-
-	handlePageTransition() {
-		const transitionName = this.props.location.state ? this.props.location.state.transition : false;
-		if (transitionName) {
-			return (
-				<ReactCSSTransitionGroup
-		          	component="div"
-		          	transitionAppear={true}
-		          	transitionName={this.props.location.state ? this.props.location.state.transition : 'default'}
-		          	transitionAppearTimeout={500}
-		          	transitionEnterTimeout={1000}
-		          	transitionLeaveTimeout={1000}
-		        >
-		          	{React.cloneElement(this.props.children, {
-		            	key: this.props.location.pathname
-		          	})}
-		        </ReactCSSTransitionGroup>
-			);
-		}
-		return this.props.children;
 	}
 
 	render() {
@@ -46,7 +24,7 @@ class App extends Component {
 				<Header />
 				<div styleName="body" className="container">
 					<div className="row">
-						{ this.handlePageTransition() }
+						{ this.props.children }
 					</div>
 				</div>
 				<Footer />
---
+++ src/containers/Entry/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+import Helmet from 'react-helmet';
+
+import * as articleActions from '../../actions/articleActions';
+import { connect } from 'react-redux';
+
+import ArticleList from '../../components/ArticleList';
+import ArticleContent from '../../components/ArticleContent';
+
+import CSSModules from 'react-css-modules';
+import styles from './styles.css';
+
+class Entry extends Component {
+
+	componentDidMount() {
+		if (this.props.articleActive.data.id != this.props.params.id) {
+			this.props.getArticleContentById(this.props.params.id);
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.params.id !== this.props.params.id) {
+			this.props.getArticleContentById(nextProps.params.id);
+		}
+	}
+
+	renderArticle() {
+		if (this.props.articleActive.error) {
+			return (
+				<div>{ this.props.articleActive.error }</div>
+			);
+		}
+		return (
+			<ArticleContent article={this.props.articleActive.data} />
+		);
+	}
+
+	render() {
+		this.transitionName = this.props.location.state ? this.props.location.state.transition : 'default';
+		return (
+			<ReactCSSTransitionGroup
+	          	component="div"
+	          	transitionAppear={ true }
+	          	transitionName={ this.transitionName }
+	          	transitionAppearTimeout={ 500 }
+	          	transitionEnterTimeout={ 1000 }
+	          	transitionLeaveTimeout={ 1000 }
+	        >
+	          	<div key={this.props.location.pathname}>
+					<Helmet 
+						title={ this.props.articleActive.data.title }
+						meta={[
+					        {
+					        	"name": "description", 
+					        	"content": "Add description here"
+					        }
+					    ]} 
+				    />
+					<div className="col-md-8">
+						{ this.renderArticle() }
+					</div>
+					<div className="col-md-4">
+						<ArticleList articles={this.props.articleActive.related} />
+					</div>
+				</div>
+	        </ReactCSSTransitionGroup>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		articleActive: state.articleActive
+	};
+}
+
+Entry.prefetchData = [
+	function(params) {
+		return articleActions.getArticleContentById(params.id);
+	}
+];
+
+module.exports = connect(mapStateToProps, articleActions)(Entry);
