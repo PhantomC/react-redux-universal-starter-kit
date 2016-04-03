a7bbdb5119e150a4fe86bfa6a2c3627255668eb0
SuranartN <suranartnc@gmail.com>
2016-04-03 18:36:00 +0700

    implement updeep

--- src/reducers/articleActiveReducer.js
+++ src/reducers/articleActiveReducer.js
@@ -1,31 +1,27 @@
-const initialData = {};
-const initialRelated = [];
+import u from 'updeep';
 
 const initialState = {
 	error: false,
-	data: initialData,
-	related: initialRelated
+	data: {},
+	related: []
 };
 
 export default function(state = initialState, action) {
 	switch(action.type) {
 		case 'GET_ARTICLE_BY_ID':
 			if (action.error) {
-				return {
-					...initialState,
+				return u({
 					error: action.error
-				};
+				}, initialState);
 			}
-			return {
-				...initialState,
+			return u({
 				data: action.data,
 				error: false
-			};
+			}, initialState);
 		case 'GET_ARTICLE_RELATED':
-			return {
-				...state,
+			return u({
 				related: action.data
-			};
+			}, state);
 		case 'GET_ARTICLE_LATEST':
 			return initialState;
 		default:
