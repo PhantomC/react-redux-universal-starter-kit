9ff559a77eaa60b9b342497390b84ab64a59ef1c
SuranartN <suranartnc@gmail.com>
2016-04-10 22:53:47 +0700

    add simple test

---
+++ src/tests/helper.js
@@ -0,0 +1,48 @@
+import _$ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import jsdom from 'jsdom';
+import chai, { expect } from 'chai';
+import chaiJquery from 'chai-jquery';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../reducers';
+
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+const $ = _$(window);
+
+// take all properties of the window object and also attach it to the 
+// mocha global object
+propagateToGlobal(global.window)
+
+function propagateToGlobal (window) {
+  for (let key in window) {
+    if (!window.hasOwnProperty(key)) continue
+    if (key in global) continue
+
+    global[key] = window[key]
+  }
+}
+
+chaiJquery(chai, chai.util, $);
+
+function renderComponent(ComponentClass, props = {}, state = {}) {
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={ createStore(reducers, state) }>
+            <ComponentClass {...props } />
+        </Provider>
+    );
+
+    return $(ReactDOM.findDOMNode(componentInstance));
+}
+
+$.fn.simulate = function(eventName, value) {
+    if (value) {
+        this.val(value);
+    }
+    TestUtils.Simulate[eventName](this[0]);
+};
+
+export { renderComponent, expect };
