b429a271a7476250a3cc32070ed5e49d3dd07351
TaToonz <peerapath.t@gmail.com>
2016-04-19 14:39:54 +0700

    added fail type

--- src/shared/redux/middlewares/promiseResolver.js
+++ src/shared/redux/middlewares/promiseResolver.js
@@ -13,18 +13,23 @@ export default store => next => action => {
 
   const DONE = type;
   const REQUEST = `${type}_REQUEST`;
+  const FAIL = `${type}_FAIL`;
+
   next({...rest, type: REQUEST });
 
   return fetch(`${apiURL}${path}`, options)
     .then(function(response) {
       if (response.status >= 400) {
-        return false;
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
       }
+
       return response.json();
     })
     .then(data => {
       if (!data) {
-        next({...rest, type: DONE, error: 'Error 404 - Not Found'});
+        next({...rest, type: FAIL, error: 'Error 404 - Not Found'});
         return false;
       }
       let result = next({...rest, type: DONE, data});
@@ -34,5 +39,10 @@ export default store => next => action => {
       }
 
       return result;
+    })
+    .catch(e => {
+      return e.response.json().then(data => {
+        return next({ ...rest, type: FAIL, ...data });
+      });
     });
 };
