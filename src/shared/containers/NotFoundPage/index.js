8867c27c8fe79b324174c203602b1150935d50d8
Thongchai Kitiyanantawong <mypumz@gmail.com>
2016-05-20 23:09:08 +0700

    edit 404 messege

--- src/shared/containers/NotFoundPage/index.js
+++ src/shared/containers/NotFoundPage/index.js
@@ -14,7 +14,7 @@ const NotFound = function() {
         <div styleName="container">
           <h1 styleName="title">404</h1>
           <h3 styleName="sub-title">Page Not Found</h3>
-          <p styleName="messege">Sorry, but the page you are looking for has note been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.</p>
+          <p styleName="messege">Sorry, but the page you are looking for has not been found. Try checking the URL for error, then hit the refresh button on your browser or try found something else in our app.</p>
         </div>
       </div>
     </div>
