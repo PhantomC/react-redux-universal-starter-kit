737a33e9b67aec789cdbd23b698e25f9011b66df
Suranart Niamcome <suranartnc@gmail.com>
2016-06-06 19:45:15 +0700

    fix static path

--- src/server/index.js
+++ src/server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
-app.use(express.static(__dirname + 'static'));
+app.use(express.static('static'));
 app.use(routeHandlers);
 
 // json-server
